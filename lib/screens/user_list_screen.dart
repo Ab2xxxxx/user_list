8e21dfe305ee8ffa614bf9987443b8ade6ab643e
Abdukholiq Tojiboev <tojiboevabdukholiq@gmail.com>
2025-01-09 15:40:52 +0300

    first commit

--- lib/screens/user_list_screen.dart
+++ lib/screens/user_list_screen.dart
@@ -13,6 +13,7 @@ class UserListScreen extends StatefulWidget {
 
 class _UserListScreenState extends State<UserListScreen> {
   String _filterText = '';
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,72 +22,109 @@ class _UserListScreenState extends State<UserListScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('User List'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.filter_list),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text('Фильтрация по имени'),
-                  content: TextField(
-                    decoration: InputDecoration(hintText: 'Введите имя'),
-                    onChanged: (value) {
-                      setState(() {
-                        _filterText = value;
-                      });
-                    },
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: Text('Закрыть'),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Provider.of<ThemeProvider>(context).isDarkMode ? Icons.wb_sunny : Icons.brightness_2),
-            onPressed: () {
-              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.refresh), 
-            onPressed: (){
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Поиск...',
+              suffixIcon: IconButton(
+                icon: Icon(Icons.clear),
+                onPressed: () {
+                  setState(() {
+                    _searchController.clear();
+                    _filterText = '';
+                  });
+                }
+              ),
+              prefixIcon: IconButton(
+                icon: Icon(Provider.of<ThemeProvider>(context).isDarkMode ? Icons.wb_sunny_outlined : Icons.brightness_2_outlined),
+                onPressed: () {
+                  Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+                },
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+            onEditingComplete: () {
               setState(() {
-                userProvider.fetchUsers(); 
-                _filterText = '';
+                _filterText = _searchController.text;
               });
-            } 
+            },
           ),
-        ],
+        )
       ),
-      body: userProvider.isLoading
-          ? Center(child: CircularProgressIndicator())
-          : userProvider.error.isNotEmpty
-              ? Center(child: Text(userProvider.error))
-              : ListView.builder(
-                  itemCount: filteredUsers.length,
-                  itemBuilder: (context, index) {
-                    final user = filteredUsers[index];
-                    return ListTile(
-                      title: Text(user.name),
-                      subtitle: Text(user.email),
-                      leading: Icon(Icons.person),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => UserDetailScreen(user: user),
+      // appBar: AppBar(
+      //   title: Text('User List'),
+      //   actions: [
+      //     IconButton(
+      //       icon: Icon(Icons.filter_list),
+      //       onPressed: () {
+      //         showDialog(
+      //           context: context,
+      //           builder: (context) => AlertDialog(
+      //             title: Text('Фильтрация по имени'),
+      //             content: TextField(
+      //               decoration: InputDecoration(hintText: 'Введите имя'),
+      //               onChanged: (value) {
+      //                 setState(() {
+      //                   _filterText = value;
+      //                 });
+      //               },
+      //             ),
+      //             actions: [
+      //               TextButton(
+      //                 onPressed: () => Navigator.of(context).pop(),
+      //                 child: Text('Закрыть'),
+      //               ),
+      //             ],
+      //           ),
+      //         );
+      //       },
+      //     ),
+      //     IconButton(
+      //       icon: Icon(Provider.of<ThemeProvider>(context).isDarkMode ? Icons.wb_sunny : Icons.brightness_2),
+      //       onPressed: () {
+      //         Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+      //       },
+      //     ),
+      //     IconButton(
+      //       icon: Icon(Icons.refresh), 
+      //       onPressed: (){
+      //         setState(() {
+      //           userProvider.fetchUsers(); 
+      //           _filterText = '';
+      //         });
+      //       } 
+      //     ),
+      //   ],
+      // ),
+      body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: userProvider.isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : userProvider.error.isNotEmpty
+                      ? Center(child: Text(userProvider.error))
+                      : ListView.builder(
+                          itemCount: filteredUsers.length,
+                          itemBuilder: (context, index) {
+                            final user = filteredUsers[index];
+                            return ListTile(
+                              title: Text(user.name),
+                              subtitle: Text(user.email),
+                              leading: Icon(Icons.person),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                              onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => UserDetailScreen(user: user),
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  },
-                ),
+            ),
     );
   }
 }
